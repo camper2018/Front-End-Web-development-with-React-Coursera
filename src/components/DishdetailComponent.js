41302038ce4d6f45442a18040dace562e2a6f8f4
camper2018 <taniafali@gmail.com>
2020-12-02 15:51:34 -0800

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 class DishdetailComponent extends Component {
   renderDish(dish) {
-    if (dish != null)
+    if (dish != null) {
       return (
         <Card key={dish.id}>
           <CardImg top src={dish.image} alt={dish.name} />
@@ -12,22 +12,42 @@ class DishdetailComponent extends Component {
           </CardBody>
         </Card>
       );
-    else return <div></div>;
+    } else {
+      return <div></div>;
+    }
   }
+  // renderComments(commentsArray) {
+  //   let dishComments = commentsArray.map((comment) => {
+  //     let date = new Date(comment.date);
+  //     date = date.toDateString();
+  //     date = date.split(" ");
+  //     let month = date[1];
+  //     let day = date[2];
+  //     let year = date[3];
+  //     return (
+  //       <div key={comment.id}>
+  //         <li>{comment.comment}</li>
+  //         <br></br>
+  //         <li>
+  //           -- {comment.author}, {month} {day}, {year}
+  //         </li>
+  //         <br></br>
+  //       </div>
+  //     );
+  //   });
   renderComments(commentsArray) {
     let dishComments = commentsArray.map((comment) => {
-      let date = new Date(comment.date);
-      date = date.toDateString();
-      date = date.split(" ");
-      let month = date[1];
-      let day = date[2];
-      let year = date[3];
       return (
         <div key={comment.id}>
           <li>{comment.comment}</li>
           <br></br>
           <li>
-            -- {comment.author}, {month} {day}, {year}
+            -- {comment.author},{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
           </li>
           <br></br>
         </div>
@@ -42,19 +62,21 @@ class DishdetailComponent extends Component {
   }
   render() {
     let dishComments = null;
-    if (this.props.selectedDish) {
-      dishComments = this.renderComments(this.props.selectedDish.comments);
+    if (this.props.dish) {
+      dishComments = this.renderComments(this.props.dish.comments);
     } else {
       dishComments = <div></div>;
     }
 
     return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          {this.renderDish(this.props.selectedDish)}
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <div>{dishComments}</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <div>{dishComments}</div>
+          </div>
         </div>
       </div>
     );
